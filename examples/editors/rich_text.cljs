f4cb4595e66c6bd3c9324ced730029fa91023fa7
Callum Herries <hi@callumherries.com>
2022-04-22 08:36:30 +0300

    Move demo app into separate directory

--- examples/editors/rich_text.cljs
+++ examples/editors/rich_text.cljs
@@ -1,10 +1,10 @@
-(ns rich.app
+(ns editors.rich-text
   (:require [hyperfiddle.rcf]
             [rich.core :as rich]
             [reagent.dom :as rdom]
             [clojure.pprint :as pprint]))
 
-(defn app []
+(defn editor []
   [:div {:style {:padding        "10px"
                  :min-height     "100px"
                  :display        "flex"
@@ -24,20 +24,3 @@
         rich/hickory->hiccup
         pprint/pprint
         with-out-str)]])
-
-; Enable tests after app namespaces are loaded (intended for subsequent REPL interactions)
-(set! hyperfiddle.rcf/*enabled* true)
-
-(defn ^:dev/after-load start []
-  ; prevent test execution during cljs hot code reload
-  (set! hyperfiddle.rcf/*enabled* true)
-  (js/console.log "Starting...")
-  (rdom/render [app] (js/document.getElementById "app")))
-
-(defn ^:dev/before-load stop []
-  (remove-watch rich/state :state-logger)
-  (set! hyperfiddle.rcf/*enabled* false))
-
-(defn ^:export init []
-  (enable-console-print!)
-  (start))
---
+++ examples/examples.cljs
@@ -0,0 +1,26 @@
+(ns examples
+  (:require [editors.rich-text]
+            [hyperfiddle.rcf]
+            [rich.core :as rich]
+            [reagent.dom :as rdom]))
+
+(defn root []
+  [:div
+   [editors.rich-text/editor]])
+
+; Enable tests after app namespaces are loaded (intended for subsequent REPL interactions)
+(set! hyperfiddle.rcf/*enabled* true)
+
+(defn ^:dev/after-load start []
+  ; prevent test execution during cljs hot code reload
+  (set! hyperfiddle.rcf/*enabled* true)
+  (js/console.log "Starting...")
+  (rdom/render [root] (js/document.getElementById "app")))
+
+(defn ^:dev/before-load stop []
+  (remove-watch rich/state :state-logger)
+  (set! hyperfiddle.rcf/*enabled* false))
+
+(defn ^:export init []
+  (enable-console-print!)
+  (start))
